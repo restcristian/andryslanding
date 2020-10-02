1e86f87181c857b4f3f0e6ff934946a7d0ca7945
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-10-02 22:14:17 +0200

    basic layout

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('hello andrys');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
