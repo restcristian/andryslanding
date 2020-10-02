b26ebec8272d0bff55e17e70d0ff6c8865cf4770
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-10-02 22:53:28 +0200

    remove temporarly form

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/contact', (req, res) => {
+  console.log('application sent');
+  res.status(200);
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
