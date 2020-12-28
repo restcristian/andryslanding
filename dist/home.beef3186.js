fe96e959315baba0fb85a27ec5eff13c3800ef0e
Cristian <restc@farmingdale.edu>
2020-12-28 22:13:57 +0100

    hopefully

--- dist/home.beef3186.js
+++ dist/home.beef3186.js
@@ -6665,7 +6665,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54455" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61268" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
