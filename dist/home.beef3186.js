dc2bd7c184b966f5926cbadf05f51c85b9060c15
Cristian <restc@farmingdale.edu>
2020-12-28 21:10:00 +0100

    work-fusion

--- dist/home.beef3186.js
+++ dist/home.beef3186.js
@@ -6661,7 +6661,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60144" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
