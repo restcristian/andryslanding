a8da02ee27681b439d7daaeca1f3113e7b76fc9e
Cristian <restc@farmingdale.edu>
2020-12-30 00:50:57 +0100

    sica

--- dist/home.beef3186.js
+++ dist/home.beef3186.js
@@ -6675,7 +6675,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60140" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51619" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
