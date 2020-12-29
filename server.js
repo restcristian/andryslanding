0d2a3393d77e6d116a2363e5a2a5f464a2d5b477
Cristian <restc@farmingdale.edu>
2020-12-29 01:52:58 +0100

    tsst

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'home.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   // res.send(views.landing({ message: '' }));
 });
 
