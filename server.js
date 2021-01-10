dc3b4b00cf0c1f3d65227cda3d230655341b24ec
Cristian <restc@farmingdale.edu>
2021-01-10 23:59:30 +0100

    test

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (env !== "development") {
 
   // set up a route to redirect http to https
   app.get((req, res, next) => {
+    console.log(req.headers.host);
     if (req.headers["x-forwarded-proto"] !== 'https') {
       res.redirect("https://" + req.headers.host + req.url)
     } else {
