029653287825e0226a7acc8274ad92668ba5c4fd
Cristian <restc@farmingdale.edu>
2021-01-10 23:37:30 +0100

    refactor to force https redirect

--- server.js
+++ server.js
@@ -2,58 +2,35 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const sgMail = require('@sendgrid/mail');
 const views = require('./views');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const env = process.env.NODE_ENV || "development";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+if (env !== "development") {
+
+  // set up a route to redirect http to https
+  http.get('*', function (req, res) {
+    console.log('https://' + req.headers.host + req.url);
+
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+  });
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-  // res.send(views.landing({ message: '' }));
+  res.sendFile(path.resolve(__dirname, 'dist', 'home.html'));
 });
 
-app.post('/', (req, res) => {
-  const {
-    email,
-    message
-  } = req.body;
-
-  const mailData = {
-    to: 'hi@andrysfrias.com',
-    from: 'andrysfrias@gmail.com',
-    subject: `Message from the Landing Page <${email}>`,
-    text: message,
-    html: `<strong>${message}</strong>`,
-  };
-  let result = {};
-  sgMail
-    .send(mailData)
-    .then((data) => {
-      console.log('data', data);
-      result = {
-        message: 'Gracias por su mensaje.',
-      };
-      return res.send(views.landing(result));
-    })
-    .catch((error) => {
-      console.log('error', error);
-      if (error.response) {
-        result = {
-          message: 'Su mensaje no pudo ser enviado.',
-        };
-        return res.send(views.landing(result));
-      }
-    });
-});
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
